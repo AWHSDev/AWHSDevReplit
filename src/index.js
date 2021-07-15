741dae227434e760a667b66b8c313ecec7c87619
AWHSDev <awhsdev@gmail.com>
2021-07-15 00:20:02 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import express from 'express'
+import fetch from 'node-fetch'
 
 const site = express()
+const HTTPHeaders = Object.freeze({
+  CONTENT_TYPE: 'Content-Type', ACCES_CONTROL_ALLOW_ORIGIN: 'Access-Control-Allow-Origin'
+})
+
+const ContentTypes = Object.freeze({
+  CSV: 'text/csv'
+})
 
 site.head('/status', (request, response) => {
   response.header("Access-Control-Allow-Origin", "*")
@@ -10,6 +18,14 @@ site.head('/status', (request, response) => {
 
 site.get('/status', (request, response) => {
   response.status(204)
+})
+
+site.get('/assets/dynamic/sheet.csv', async (request, response) => {
+
+  response.header(HTTPHeaders.CONTENT_TYPE, ContentTypes.CSV)
+  response.header(HTTPHeaders.ACCES_CONTROL_ALLOW_ORIGIN, 'https://awhs-dev.web.app')
+
+  response.send(await (await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_mdCaM5E7DwTqY7h3lgylU_rG7QwoSTEIT--4KDWVdqo3ekMmIMwmJcipzCqwPmBhqTPvROXcaeMI/pub?output=csv')).text())
 })
 
 site.get('/', (request, response) => {
